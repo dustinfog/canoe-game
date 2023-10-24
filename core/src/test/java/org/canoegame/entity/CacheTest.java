67b86c1bc423e0f1013e7460fc9a82fd2d71eeb3
zhandong.pan <zhandong.pan@kingsgroupgames.com>
2023-10-24 11:07:11 +0800

    optimize MockEntity class code

--- core/src/test/java/org/canoegame/entity/CacheTest.java
+++ core/src/test/java/org/canoegame/entity/CacheTest.java
@@ -16,12 +16,12 @@ public class CacheTest extends TestCase {
 
         public int revision;
 
-        private final Key key;
+        private final org.canoegame.entity.Key<MockEntity> k;
 
         public MockEntity(int key1, int key2) {
             this.key1 = key1;
             this.key2 = key2;
-            key = Key.create(key1, key2);
+            k = key(key1, key2);
         }
 
         public int getKey1() {
@@ -44,7 +44,7 @@ public class CacheTest extends TestCase {
         }
 
         public int hashCode() {
-            return key.hashCode();
+            return k.hashCode();
         }
 
         public boolean equals(Object o) {
@@ -52,97 +52,84 @@ public class CacheTest extends TestCase {
                 return false;
             }
 
-            return key.equals(e.getKey());
+            return k.equals(e.getKey());
         }
 
-        static class Key implements org.canoegame.entity.Key<MockEntity> {
-
-            private final int key1;
-            private final int key2;
-
-            private final int significant;
-
-            private Key(int key1, int key2, int significant) {
-                this.key1 = key1;
-                this.key2 = key2;
-                this.significant = significant;
-            }
-
-            public static Key create(int key1, int key2) {
-                return new Key(key1, key2, 2);
-            }
-
-            public static Key createPrefix() {
-                return new Key(0, 0, 0);
-            }
-
-            public static Key createPrefix(int key1) {
-                return new Key(key1, 0, 1);
-            }
-
-            @Override
-            public boolean isPrefixOf(org.canoegame.entity.Key<MockEntity> key) {
-                if (!(key instanceof Key k)) {
-                    return false;
-                }
-
-                if (significant == 0) {
-                    return true;
-                }
-
-                if (significant > k.significant) {
-                    return false;
-                }
-
-                for (var i = 0; i < significant; i ++) {
-                    if (compareField(k, i) != 0) {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-
-            @Override
-            public int groupCode() {
-                return key1;
-            }
-
-            public int hashCode() {
-                return key1 * 31 + key2;
-            }
-
-            public boolean equals(Object o) {
-                if (!(o instanceof Key k)) {
-                    return false;
-                }
-
-                return compareTo(k) == 0;
-            }
-
-            @Override
-            public int compareTo(@NotNull org.canoegame.entity.Key<MockEntity> o) {
-                if (!(o instanceof Key k)) {
-                    return 0;
-                }
-
-                for (int i = 0, minSig = Math.min(significant, k.significant); i < minSig; i ++) {
-                    var r = compareField(k, i);
-                    if (r != 0) {
-                        return r;
-                    }
-                }
-
-                return Integer.compare(significant, k.significant);
-            }
-
-            private int compareField(@NotNull Key k, int index) {
-                return switch (index) {
-                    case 0 -> Integer.compare(key1, k.key1);
-                    default -> Integer.compare(key2, k.key2);
-                };
-            }
+        public static org.canoegame.entity.Key<MockEntity> key(int key1, int key2) {
+            return new Key(key1, key2, 2);
         }
+        public static org.canoegame.entity.Key<MockEntity> prefix(int key1) {
+            return new Key(key1, 0, 1);
+        }
+        public static org.canoegame.entity.Key<MockEntity> prefix() {
+            return new Key(0, 0, 0);
+        }
+
+        private record Key(int key1, int key2, int significant) implements org.canoegame.entity.Key<MockEntity> {
+
+            @Override
+                    public boolean isPrefixOf(org.canoegame.entity.Key<MockEntity> key) {
+                        if (!(key instanceof Key k)) {
+                            return false;
+                        }
+
+                        if (significant == 0) {
+                            return true;
+                        }
+
+                        if (significant > k.significant) {
+                            return false;
+                        }
+
+                        for (var i = 0; i < significant; i++) {
+                            if (compareField(k, i) != 0) {
+                                return false;
+                            }
+                        }
+
+                        return true;
+                    }
+
+                    @Override
+                    public int groupCode() {
+                        return key1;
+                    }
+
+                    public int hashCode() {
+                        return key1 * 31 + key2;
+                    }
+
+                    public boolean equals(Object o) {
+                        if (!(o instanceof Key k)) {
+                            return false;
+                        }
+
+                        return compareTo(k) == 0;
+                    }
+
+                    @Override
+                    public int compareTo(@NotNull org.canoegame.entity.Key<MockEntity> o) {
+                        if (!(o instanceof Key k)) {
+                            return 0;
+                        }
+
+                        for (int i = 0, minSig = Math.min(significant, k.significant); i < minSig; i++) {
+                            var r = compareField(k, i);
+                            if (r != 0) {
+                                return r;
+                            }
+                        }
+
+                        return Integer.compare(significant, k.significant);
+                    }
+
+                    private int compareField(@NotNull Key k, int index) {
+                        return switch (index) {
+                            case 0 -> Integer.compare(key1, k.key1);
+                            default -> Integer.compare(key2, k.key2);
+                        };
+                    }
+                }
 
         public enum Field implements org.canoegame.entity.Field {
             KEY_1(1),
@@ -166,7 +153,7 @@ public class CacheTest extends TestCase {
 
         @Override
         public org.canoegame.entity.Key<MockEntity> getKey() {
-            return key;
+            return k;
         }
 
         @Override
@@ -187,9 +174,9 @@ public class CacheTest extends TestCase {
 
     public void testPutNullIfAbsent() {
         var cache = new Cache<>(MockEntity.class, TimeUnit.SECONDS.toMillis(1), true);
-        var holder = cache.putNullIfAbsent(MockEntity.Key.create(1, 2));
-        var holder1 = cache.putNullIfAbsent(MockEntity.Key.create(1, 2));
-        var holder3 = cache.get(MockEntity.Key.create(1, 2));
+        var holder = cache.putNullIfAbsent(MockEntity.key(1, 2));
+        var holder1 = cache.putNullIfAbsent(MockEntity.key(1, 2));
+        var holder3 = cache.get(MockEntity.key(1, 2));
 
         assertSame(holder, holder1);
         assertSame(holder, holder3);
@@ -208,24 +195,24 @@ public class CacheTest extends TestCase {
         cache.putOnStore(e3, false);
 
 
-        var all = cache.getAll(MockEntity.Key.createPrefix(1));
+        var all = cache.getAll(MockEntity.prefix(1));
         assertNull(all);
 
-        cache.putPrefix(MockEntity.Key.createPrefix(1));
-        all = cache.getAll(MockEntity.Key.createPrefix(1));
+        cache.putPrefix(MockEntity.prefix(1));
+        all = cache.getAll(MockEntity.prefix(1));
         assertSame(3, all.size());
         assertSame(e1, all.get(0).get());
         assertSame(e2, all.get(1).get());
         assertSame(e3, all.get(2).get());
 
-        var key = MockEntity.Key.create(1, 8);
+        var key = MockEntity.key(1, 8);
         assertNotNull(cache.get(key));
         assertNull(cache.get(key).get());
 
         var e4 = new MockEntity(2, 1);
         cache.putOnStore(e4, false);
-        cache.putPrefix(MockEntity.Key.createPrefix(2));
-        var all2 = cache.getAll(MockEntity.Key.createPrefix(2));
+        cache.putPrefix(MockEntity.prefix(2));
+        var all2 = cache.getAll(MockEntity.prefix(2));
         assertSame(1, all2.size());
 
 
@@ -233,11 +220,11 @@ public class CacheTest extends TestCase {
         cache.putOnStore(e1, true);
         cache.putOnStore(e2, true);
         cache.putOnStore(e3, true);
-        cache.putPrefix(MockEntity.Key.createPrefix(1));
-        var all3 = cache.getAll(MockEntity.Key.createPrefix(1));
+        cache.putPrefix(MockEntity.prefix(1));
+        var all3 = cache.getAll(MockEntity.prefix(1));
         assertSame(3, all3.size());
         Thread.sleep(1000);
-        var all4 = cache.getAll(MockEntity.Key.createPrefix(1));
+        var all4 = cache.getAll(MockEntity.prefix(1));
         assertNull(all4);
     }
 
@@ -245,7 +232,7 @@ public class CacheTest extends TestCase {
         var cache = new Cache<>(MockEntity.class, TimeUnit.SECONDS.toMillis(1), true);
         var e1 = new MockEntity(1, 2);
         cache.putOnStore(e1, false);
-        var holder = cache.get(e1.key);
+        var holder = cache.get(e1.getKey());
         assertEquals(e1, holder.get());
 
         var e2 = new MockEntity(1, 2);
@@ -259,7 +246,7 @@ public class CacheTest extends TestCase {
         if (expectedException == null) {
             fail("Expected exception not thrown");
         }
-        var holder2 = cache.get(e2.key);
+        var holder2 = cache.get(e2.getKey());
         assertSame(e1, holder2.get());
     }
 
@@ -267,7 +254,7 @@ public class CacheTest extends TestCase {
         var cache = new Cache<>(MockEntity.class, TimeUnit.SECONDS.toMillis(1), true);
         var e1 = new MockEntity(1, 2);
         cache.putOnStore(e1, false);
-        var holder = cache.get(e1.key);
+        var holder = cache.get(e1.getKey());
         assertEquals(e1, holder.get());
 
         var e2 = new MockEntity(1, 2);
@@ -281,7 +268,7 @@ public class CacheTest extends TestCase {
         if (expectedException == null) {
             fail("Expected exception not thrown");
         }
-        var holder2 = cache.get(e2.key);
+        var holder2 = cache.get(e2.getKey());
         assertEquals(e2, holder2.get());
     }
 
@@ -291,11 +278,11 @@ public class CacheTest extends TestCase {
         var e1 = new MockEntity(1, 2);
         cache.putOnStore(e1, false);
 
-        assertEquals(e1, cache.get(e1.key).get());
+        assertEquals(e1, cache.get(e1.getKey()).get());
 
         cache.putOnDelete(e1);
-        assertNotNull(cache.get(e1.key));
-        assertNull(cache.get(e1.key).get());
+        assertNotNull(cache.get(e1.getKey()));
+        assertNull(cache.get(e1.getKey()).get());
     }
 
     class MockStringKey implements Key<Integer> {
@@ -334,7 +321,7 @@ public class CacheTest extends TestCase {
     }
 
     public void testManualPrefix() {
-        var prefix = MockEntity.Key.createPrefix();
+        var prefix = MockEntity.prefix();
 
         var cache = new Cache.ManualPrefix<Integer>();
 
